2ab6a96275ed518930e20ae2f483846ac89a7006
jhsharp <73131537+jhsharp@users.noreply.github.com>
2021-10-27 16:52:34 -0500

    Music in GameManager

--- Spooky Game/Assets/Scripts/GameManager.cs
+++ Spooky Game/Assets/Scripts/GameManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
-
+using UnityEngine.Audio;
 
 
 public class GameManager : MonoBehaviour
@@ -33,9 +33,19 @@ public class GameManager : MonoBehaviour
     private static int karma;
     [SerializeField] private TextAsset saveFile;
 
+    [SerializeField] private AudioSource musicGood;
+    [SerializeField] private AudioSource musicEvil1;
+    [SerializeField] private AudioSource musicEvil2;
+    [SerializeField] private AudioSource musicEvil3;
+    [SerializeField] private AudioSource currentMusic;
+    public int x = 0;
+    public bool flag = true;
+
     void Awake()
     {
         karma = PlayerPrefs.GetInt("Karma");
+        currentMusic = musicGood;
+        currentMusic.Play();
     }
     
     // Start is called before the first frame update
@@ -47,7 +57,14 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (x >= 10000 && flag)
+        {
+            flag = false;
+            currentMusic.Stop();
+            currentMusic = musicEvil1;
+            currentMusic.Play();
+        }
+        else x += 1;
     }
 
     void ResetData()
